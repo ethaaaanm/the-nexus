8c43ab0e72531411e461f7934de7d25a142e1ca4
ethanmah <ett.mah@gmail.com>
2024-11-28 03:43:15 -0500

    Event List

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,14 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { db } from "../firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
-import './home.css';
-
-import SportSelection from "../components/SportsSelection";
-import SoftballStats from "../components/SoftballStats";
-import BasketballStats from "../components/BasketballStats";
-import VolleyballStats from "../components/VolleyballStats";
-import UltimateStats from "../components/UltimateStats";
-
+import React, { } from "react";
 import Trailer from "./Trailer";
 import photo1 from "../res/images/DSC_1405.jpg"
 import photo2 from "../res/images/DSC_1176.jpg"
@@ -17,55 +7,16 @@ import Carousel from 'react-bootstrap/Carousel';
 import CarouselItem from 'react-bootstrap/CarouselItem'
 import CarouselCaption from 'react-bootstrap/CarouselCaption'
 
+import './home.css';
+import time from "../res/images/time.png"
+
+
 const Home = () => {
-  const [selectedSport, setSelectedSport] = useState("Basketball");
-  const [playerStats, setPlayerStats] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const playersRef = collection(db, "players");
-      const playerSnapshot = await getDocs(playersRef);
-
-      const stats = playerSnapshot.docs.map((doc) => {
-        const data = doc.data();
-        const playerStats = {
-          playerName: data.playerName || "-",
-          teamId: data.team_id || "-",
-          ...data.stats[selectedSport.toLowerCase()],
-        };
-
-        if (selectedSport === "Softball") {
-          playerStats.battingAverage = data.stats.softball
-            ? ((playerStats.hits / playerStats.atBats) * 100 || 0).toFixed(2)
-            : "-";
-        }
-
-        if (selectedSport === "Volleyball") {
-          playerStats.winPercentage = data.stats.volleyball
-            ? ((playerStats.wins / playerStats.gamesPlayed) * 100 || 0).toFixed(2)
-            : "-";
-        }
-
-        for (let key in playerStats) {
-          if (playerStats[key] === null || playerStats[key] === undefined) {
-            playerStats[key] = "-";
-          }
-        }
-
-        return playerStats;
-      });
-
-      setPlayerStats(stats);
-    };
-
-    fetchData();
-  }, [selectedSport]);
-
   return (
     <div id="home">
       <h1 id="greeting">Welcome to The Nexus League!</h1>
-      <div id="clip">     
-        <Trailer/>
+      <div id="clip">
+        <Trailer />
       </div>
 
       <h2 id="feature">Features</h2>
@@ -75,39 +26,123 @@ const Home = () => {
           <CarouselItem>
             <img src={photo1} alt="First slide" />
             <CarouselCaption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Jhon Tracey</h3>
+              <p>The Nexus</p>
             </CarouselCaption>
           </CarouselItem>
           <CarouselItem>
             <img src={photo2} alt="Second slide" />
             <CarouselCaption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3>Crodey</h3>
+              <p>Big Hitta</p>
             </CarouselCaption>
           </CarouselItem>
           <CarouselItem>
             <img src={photo3} alt="Third slide" />
             <CarouselCaption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
+              <h3>Jamesie</h3>
+              <p>Elegant Bird</p>
             </CarouselCaption>
           </CarouselItem>
         </Carousel>
       </div>
 
-      <h3 id="home-title">Schedule</h3>
+      <h3 id="home-title">Schedule or Upcoming Events</h3>
 
-      <div className="sport-selection">
-        <SportSelection onSelectSport={setSelectedSport} />
-      </div>
-      <div style={{ marginTop: "20px" }}>
-        {selectedSport === "Basketball" && <BasketballStats playerStats={playerStats} />}
-        {selectedSport === "Softball" && <SoftballStats playerStats={playerStats} />}
-        {selectedSport === "Volleyball" && <VolleyballStats playerStats={playerStats} />}
-        {selectedSport === "Ultimate" && <UltimateStats playerStats={playerStats} />}
+      <div class="event-container">
+        <h3 class="year">2025</h3>
+
+        <div class="event">
+          <div class="event-left">
+            <div class="event-date">
+              <div class="date">4</div>
+              <div class="month">June</div>
+            </div>
+          </div>
+
+          <div class="event-right">
+            <h3 class="event-title">Ultimate Frisbee</h3>
+
+            <div class="event-description">
+              Week 1: Welcome to the Nexus League!
+              <br/>
+              Kick Off Event
+            </div>
+
+            <div class="event-timing">
+              <img src={time} alt="" /> 7:00 pm
+            </div>
+          </div>
+        </div>
+
+        <div class="event">
+          <div class="event-left">
+            <div class="event-date">
+              <div class="date">11</div>
+              <div class="month">June</div>
+            </div>
+          </div>
+
+          <div class="event-right">
+            <h3 class="event-title">Softball</h3>
+
+            <div class="event-description">
+              Week 2: First Softball Game<br/>Rock vs United
+            </div>
+
+            <div class="event-timing">
+              <img src={time} alt="" /> 7:00 pm
+            </div>
+          </div>
+        </div>
+
+        <div class="event">
+          <div class="event-left">
+            <div class="event-date">
+              <div class="date">18</div>
+              <div class="month">June</div>
+            </div>
+          </div>
+
+          <div class="event-right">
+            <h3 class="event-title">Basketball</h3>
+
+            <div class="event-description">
+              Week 3: Big Balls Sesh
+              <br/>
+              Bring the BOOM
+            </div>
+
+            <div class="event-timing">
+              <img src={time} alt="" /> 7:00 pm
+            </div>
+          </div>
+        </div>
+
+
+        <div class="event">
+          <div class="event-left">
+            <div class="event-date">
+              <div class="date">25</div>
+              <div class="month">June</div>
+            </div>
+          </div>
+
+          <div class="event-right">
+            <h3 class="event-title">Volleyball</h3>
+
+            <div class="event-description">
+              Week 4: Yungins Time to Shine
+              <br/>
+              Thunda Thunda!!
+            </div>
+
+            <div class="event-timing">
+              <img src={time} alt="" /> 7:00 pm
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
