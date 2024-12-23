1f143d928a4de4ec4f2b3bdddb53d2fdd2151334
ethanmah <ett.mah@gmail.com>
2024-12-23 04:36:57 -0500

    home page prep

---
+++ src/error/404.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Error404.css"; 
+
+function ErrorPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="error-page">
+      <div className="content-wrapper">
+        <h1 className="error-title">ERROR 404</h1>
+        <h4 className="error-description">YOU GOT CROSSED UP INTO THE ABYSS.</h4>
+        <div className="divider"></div>
+        <button className="back-home-button" onClick={() => navigate("/")}>
+          <h5 className="button-text">BACK HOME</h5>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,84 +1,64 @@
-import React, { } from "react";
-import Trailer from "./Trailer";
-import photo1 from "../res/images/DSC_1405.jpg"
-import photo2 from "../res/images/DSC_1176.jpg"
-import photo3 from "../res/images/DSC_1542.jpg"
-import Carousel from 'react-bootstrap/Carousel';
-import CarouselItem from 'react-bootstrap/CarouselItem'
-import CarouselCaption from 'react-bootstrap/CarouselCaption'
+import React from "react";
+import "./home.css";
 
-import './home.css';
-import events from "../data/events";
-import time from "../res/images/time.png";
+const newsData = [
+  {
+    title: "Next Game Location",
+    content: "Our next game will be hosted at People's Christian Academy, Renfrew Dr, Markham.",
+    date: "6/4/2025",
+  },
+  {
+    title: "Game Recording",
+    content: "Watch the recording on our YouTube channel: @TheNexusLeague",
+  },
+  {
+    title: "Congrats to Team Genesis!",
+    content: "For winning the first-ever game of The Nexus!",
+  },
+];
 
+const scheduleData = [
+  {
+    sport: "Ultimate Frisbee",
+    date: "6/4/2025 | 7:00PM",
+    teams: ["Genesis 1-0-0", "Refined 0-1-0"],
+  },
+  {
+    sport: "Softball",
+    date: "6/11/2025 | 7:00PM",
+    teams: ["Genesis 0-0-0", "Refined 0-0-0"],
+  },
+];
 
 const Home = () => {
   return (
-    <div id="home">
-      <h1 id="greeting">WELCOME TO <br/> THE NEXUS LEAGUE</h1>
-      <div id="clip">
-        <Trailer />
-      </div>
-
-      <h2 id="feature">Features</h2>
-
-      <div id="feature-carousel">
-        <Carousel>
-          <CarouselItem>
-            <img src={photo1} alt="First slide" />
-            <CarouselCaption>
-              <h3>Jhon Tracey</h3>
-              <p>The Nexus</p>
-            </CarouselCaption>
-          </CarouselItem>
-          <CarouselItem>
-            <img src={photo2} alt="Second slide" />
-            <CarouselCaption>
-              <h3>Crodey</h3>
-              <p>Big Hitta</p>
-            </CarouselCaption>
-          </CarouselItem>
-          <CarouselItem>
-            <img src={photo3} alt="Third slide" />
-            <CarouselCaption>
-              <h3>Jamesie</h3>
-              <p>Elegant Bird</p>
-            </CarouselCaption>
-          </CarouselItem>
-        </Carousel>
-      </div>
-
-      <h3 id="home-title">Schedule or Upcoming Events</h3>
-
-      <div className="event-container">
-        <h3 className="year">2025</h3>
-
-        {events.map((event, index) => (
-          <div className="event" key={index}>
-            <div className="event-left">
-              <div className="event-date">
-                <div className="date">{event.date}</div>
-                <div className="month">{event.month}</div>
-              </div>
-            </div>
-            <div className="event-right">
-              <h3 className="event-title">{event.title}</h3>
-              <div
-                className="event-description"
-                dangerouslySetInnerHTML={{ __html: event.description }}
-              ></div>
-              <div className="event-timing">
-                <img src={time} alt="Time icon" />
-                {event.time}
-              </div>
-            </div>
-            <div className="event-image">
-              <img src={event.image} alt={`${event.title} img`} />
-            </div>
+    <div className="home">
+    <div className="container">
+      <div className="news-section">
+        <h2>News</h2>
+        {newsData.map((item, index) => (
+          <div className="news-item" key={index}>
+            <h3>{item.title}</h3>
+            {item.date && <p>{item.date}</p>}
+            <p>{item.content}</p>
           </div>
         ))}
       </div>
 
+      <div className="schedule-section">
+        <h2>Schedule</h2>
+        {scheduleData.map((item, index) => (
+          <div className="schedule-item" key={index}>
+            <div>
+              <h3>{item.sport}</h3>
+              <p>{item.date}</p>
+              <p>{item.teams.join(" vs ")}</p>
+            </div>
+            <button className="play-button">▶</button>
+          </div>
+        ))}
+      </div>
+    </div>
     </div>
   );
 };
