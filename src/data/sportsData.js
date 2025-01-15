be357e2213f32efef59dcef288173600fda4f257
ethanmah <ett.mah@gmail.com>
2025-01-15 14:41:29 -0500

    Pushing Alex data and nav fix

--- src/data/sportsData.js
+++ src/data/sportsData.js
@@ -96,6 +96,30 @@ export const sportsStatsDB = {
                 strikeouts: [2, 1, 1]
             }
         },
+        "Alex Wong": {
+            "Team": "Refined",
+            "Abbrev": "REF",
+            "Ultimate Frisbee": {
+                points: [1, 1, 1],
+                assists: [2, 2, 2],
+                blocks: [3, 3, 3],
+            },
+            "Basketball": {
+                points: [0, 2, 1],
+                assists: [2, 3, 4],
+                rebounds: [1, 1, 1],
+            },
+            "Volleyball": {
+                wins: 0,
+                losses: 3,
+            },
+            "Softball": {
+                atBats: [2, 2, 0],
+                hits: [4, 1, 2],
+                rbi: [0, 1, 0],
+                strikeouts: [10, 10, 10]
+            }
+        },
     }
 
 }
